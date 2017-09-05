84ee84a32749ad3939df0ecc5c29156af737e74d
mvicariaf <amivicaria@hotmail.es>
2017-09-05 18:57:55 +0200

    funcion para añadir al carrito

--- js/app.js
+++ js/app.js
@@ -1,39 +1,44 @@
-
 function getMenu (food) {
-
 	 $.get('http://localhost:3000/menu', function (response){  
-	 	var total=0;
-	 	var suma=0;
+	 	
 	 	for (var i = 0; i < response[food].length; i++) {
 	 		var plato = response[food][i].nombre;
-	 		var dish = response[food][i].nombre.split(' ').join('')
-	 		var precio = response[food][i].precio
-	 		$('#'+food).append("<li><h3>"+plato+"</h3><button data-price='"+precio+"' class="+dish+">Add</button></li>");
+	 		var clasePlato = response[food][i].nombre.split(' ').join('');
+	 		var precio = response[food][i].precio;
+	 		
+	 		$('#'+food).append("<li><h3>"+plato+"</h3><button data-name='"+plato+"' data-price='"+precio+"' class='"+clasePlato+" botonPlato'>Add</button></li>");
 	 		
 	 		$('#'+food).append("<li>"+precio+" €</li>");
 	 		
-	 		
-	 		$('.'+dish).on("click", function (e) {
-				    e.preventDefault();
-				    total += añadirPrecio(dish);
-				    $("#cuenta").text(total);
-				});
 	 		if (response[food][i].descripcion  && response[food][i].imagen ) {
 	 			$('#'+food).append("<li>"+response[food][i].descripcion+"</li>");
 	 			$('#'+food).append("<img src="+response[food][i].imagen+">");
-	 		}
+	 		};
+
+	 		//$('.'+clasePlato).on('click', addItem(event, clasePlato, precio));
 	 	};
+
+	 	$('.botonPlato').on('click', function(){
+	 		var nombre = $(this).attr("data-name");
+	 		var precio = $(this).attr("data-price");
+	 		
+	 		addItem(nombre, precio);
+	 	});
 	});
-};
+}
+function addItem (clasePlato, precio) {
+   console.log("asd");
+    $('#items').append("<li>"+clasePlato+"  "+precio+"</li>");
+}
 
-
-function añadirPrecio(plato){
+/*function anadirPrecio(plato){
 	
 	var precio = parseFloat($("."+plato).attr("data-price"));
 	total =+precio
 	return total;
 	
-};
+};*/
+
 getMenu("entrantes");
 getMenu("carnes");
 getMenu("pescados");
